4ef18fc95fe87420494e977b39bda7e3a5921972
Thomas <thomas.winkler@iggmp.net>
2020-05-10 10:55:53 +0200

    initial state

---
+++ src/can_com.cpp
@@ -0,0 +1,219 @@
+/*
+ * LOCO-CAN can class
+ * 
+ * @author: Thomas H Winkler
+ * @copyright: 2020
+ * @lizence: GG0
+ */
+
+/*
+ * create can communication
+ */
+
+#include <CAN.h>
+#include <ArduinoUniqueID.h>
+#include <RokkitHash.h>
+
+#include "can_com.h"
+#include "simpletimeout.h"
+
+#define DEBUG
+
+/*
+ * create CAN communication
+ * user standard CS (10) and INT (2) ports
+ */
+CAN_COM::CAN_COM() {
+  create_uuid();
+}
+
+
+/*
+ * create CAN communication
+ * set CS and INT ports
+ */
+CAN_COM::CAN_COM(uint8_t CS, uint8_t INT) {
+  CAN.setPins(CS, INT);
+  create_uuid();
+}
+
+
+
+bool CAN_COM::begin(long speed) {
+
+  // start the CAN bus
+  #ifdef DEBUG
+    Serial.print("Start CAN at ");
+    Serial.print(speed);
+    Serial.println(" bps");
+  #endif
+  
+  while (!CAN.begin(speed)) {
+
+    #ifdef DEBUG
+      Serial.println("Starting CAN failed!");
+    #endif
+
+    delay(1000);
+  }
+
+  #ifdef DEBUG
+    Serial.println("OK!");
+  #endif
+
+  _filter_count = 0;
+
+  set_alive(CAN_ALIVE_TIMEOUT);
+
+  return true;
+}
+
+
+void CAN_COM::create_uuid(void) {
+  _uuid = rokkit((char*) UniqueID8, 8) & 0x3FFFF;
+}
+
+
+long CAN_COM::uuid(void) {
+  return _uuid;
+}
+
+
+/*
+ * set alive timeout
+ */
+void CAN_COM::set_alive(uint16_t alive_timeout) {
+  _alive_timeout.begin(alive_timeout);
+}
+
+
+/*
+ * check if CAN communication alive
+ * timeout 
+ */
+bool CAN_COM::alive(void) {
+  return _alive;
+}
+
+/*
+ * send data package
+ */
+bool CAN_COM::send(uint8_t* data, uint8_t length, uint32_t id) {
+
+  uint8_t i = 0;
+
+  // begin packet
+  // use 29 bit identifier
+  // 11 bit: id
+  // 18 bit: board uuid
+  CAN.beginExtendedPacket((id << 18) | _uuid);
+
+
+  // send data with length
+  // restrict to 8 uint8_ts
+  while (i < length && i < 8) {
+    CAN.write(data[i++]);  
+  }
+  
+  // CAN.endPacket();
+  CAN.endPacket();
+
+  return true;
+}
+
+
+/*
+ * read data, return true if filter
+ */
+uint16_t CAN_COM::read(CAN_MESSAGE* message) {
+
+  uint8_t i;
+  uint8_t size;
+  uint32_t can_id;
+
+
+  // check and connection and update alive status
+  _alive = !_alive_timeout.check();
+
+
+  // ===============================================
+  // check for package
+  size = CAN.parsePacket();
+
+
+  // received a packet
+  if (size) {
+
+
+    // retrigger connection timeout
+    _alive_timeout.retrigger();
+
+
+    // fetch data
+    i = 0;        
+    while (CAN.available() && i < 8) {
+
+      message->data[i++] = (uint8_t)CAN.read();
+    }
+
+    // add size
+    message->size = i;
+    
+    // get packet id
+    // is extended
+    // split in group id (11 bit) and uuid (18 bit)
+    if (CAN.packetExtended()) {
+
+      can_id = CAN.packetId();
+
+      message->id = can_id >> 18;
+      message->uuid = can_id & 0x3FFFF;
+    }
+
+
+    else {
+      message->id = CAN.packetId();
+      message->uuid = 0;
+    }
+    
+
+    // check for filter criteriy
+    if (_filter_count == 0) {
+      return true;
+    }
+
+    // check for registered filters
+    i = 0;
+    while (i < _filter_count) {
+
+      // filter found
+      if ((message->id & _masks[i]) == _filters[i]) {
+        return true;
+      }
+
+      i++;
+    }
+  }
+
+  return false;
+}
+
+
+/*
+ * filter packet id
+ true if valid
+ */
+bool CAN_COM::register_filter(uint16_t mask, uint16_t filter) {
+
+  // has free filter slots
+  if (_filter_count < CAN_MAX_FILTER) {
+
+    _masks[_filter_count] = mask;
+    _filters[_filter_count] = filter;
+
+    _filter_count++;
+
+  }
+
+  return _filter_count;
+}
---
+++ src/can_com.h
@@ -0,0 +1,79 @@
+/*
+ * LOCO-CAN can class header file
+ * 
+ * @author: Thomas H Winkler
+ * @copyright: 2020
+ * @lizence: GG0
+ * 
+ * using can library:
+ * https://github.com/sandeepmistry/arduino-CAN
+ * https://github.com/ricaun/ArduinoUniqueID
+ * https://github.com/SukkoPera/Arduino-Rokkit-Hash
+ */
+
+#ifndef CAN_COM_H
+#define CAN_COM_H
+
+#include <CAN.h>
+#include <simpletimeout.h>
+
+
+/*
+ * CAN speeds
+ * 
+ * 1000 bps = 1000E3
+ * 500 bps = 500E3
+ * ...
+ * 
+ * valid speed settings
+ * 1000, 500, 250, 200, 125, 100, 80, 50, 40, 20, 10 ,5
+ */
+
+
+#define CAN_MAX_FILTER 8
+#define CAN_ALIVE_TIMEOUT 500
+
+
+struct CAN_MESSAGE {
+    uint32_t id;
+    uint16_t uuid;
+    uint8_t data[8];
+    uint8_t size;
+};
+
+
+
+class CAN_COM {
+
+  public:
+    CAN_COM(void); // construct with default CS and INT port
+    CAN_COM(uint8_t CS, uint8_t INT); // user CS and INT ports
+
+    bool begin(long speed); // start communication with speed setting
+    bool alive(void);
+    void set_alive(uint16_t alive_timeout); // set alive timeout
+
+    bool send(byte* data, byte length, uint32_t id); // send data
+    
+    bool register_filter(uint16_t mask, uint16_t filter); // add mask and filter
+    uint16_t read(CAN_MESSAGE* message); // receive data > true if no filter or filter match
+
+    long uuid(void);
+
+  private:
+
+    void create_uuid(void);
+
+    long _uuid;
+
+    uint16_t _masks[CAN_MAX_FILTER];
+    uint16_t _filters[CAN_MAX_FILTER];
+
+    uint8_t _filter_count;
+
+    SIMPLETIMEOUT _alive_timeout;
+    bool _alive;
+
+};
+
+#endif
